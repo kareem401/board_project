833250f9c69a0358eec72dab63eba44046ecdd54
kareem401 <69206156+kareem401@users.noreply.github.com>
2020-08-13 15:59:08 +0200

    Add files via upload

--- topic/urls.py
+++ topic/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
 from . import views
-from .views import TopicListView, TopicDetailView
+from .views import TopicListView, TopicDetailView, TopicCreateView, TopicUpdateView, TopicDeleteView
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
+	path('post/<int:pk>/delete/', TopicDeleteView.as_view(), name='post_delete'),
+	path('post/<int:pk>/edit/', TopicUpdateView.as_view(), name='post_edit'),
+	path('post/new/', TopicCreateView.as_view(), name="post_new"),
    	path('', TopicListView.as_view(), name='home'),
    	path('post/<int:pk>/', TopicDetailView.as_view(), name='post_detail'),
+   	path('', TopicCreateView.as_view(), name='home'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- topic/views.py
+++ topic/views.py
@@ -1,5 +1,8 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+
 class TopicListView(ListView):
 	model = Post
 	template_name = 'home.html'
@@ -7,3 +10,15 @@ class TopicListView(ListView):
 class TopicDetailView(DetailView):
 	model = Post
 	template_name = "post_detail.html"
+class TopicCreateView(CreateView):
+	model = Post
+	template_name = "post_new.html"
+	fields = "__all__"
+class TopicUpdateView(UpdateView):
+	model = Post
+	template_name="post_edit.html"
+	fields = ["title","body"]
+class TopicDeleteView(DeleteView):
+	model = Post
+	template_name = "post_delete.html"
+	success_url = reverse_lazy("home")
